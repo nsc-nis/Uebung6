518da7312da8865bf9a4199633e581869b04b1c0
ThomasRiedl <thomas-riedl@outlook.com>
2020-11-03 18:07:33 +0100

    commit progressbar

---
+++ src/Main/Riedl.java
@@ -0,0 +1,33 @@
+package Main;
+
+import javafx.scene.control.ProgressIndicator;
+
+public class GaugeAdvanced extends GaugeBase
+{
+    private int value;
+
+    public GaugeAdvanced() {
+    }
+    @Override
+    public void redraw()
+    {
+        double progressValue = (double)value;
+        System.out.println(progressValue);
+        double progress = progressValue / 100;
+
+        ProgressIndicator progressIndicator = new ProgressIndicator(0.0);
+        progressIndicator.setPrefSize(50000, 50000);
+        progressIndicator.setProgress(progress);
+        System.out.println(progress);
+
+        this.getChildren().addAll(progressIndicator);
+
+    }
+
+    @Override
+    public void setValue(int v)
+    {
+        this.value = v;
+        this.redraw();
+    }
+}
